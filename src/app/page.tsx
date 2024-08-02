97b37a8b7e1c3180b767cc68873a6cc283b05346
aibekovislam <islamxdaamduu@gmail.com>
2024-08-02 17:34:43 +0600

    changed desing of project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-import Image from "next/image";
 import NavigationNavbar from "@/components/Navbar/NavigationNavbar";
-import { Button, ConfigProvider, Flex } from "antd";
+import { ConfigProvider, Flex } from "antd";
 import localFont from 'next/font/local';
-import ArrowRight from './assets/svgs/arrow_right.svg';
 import SectionCarousel from "@/sections/SectionCarousel";
 import SectionList from "@/sections/SectionList";
 import SectionThree from "@/sections/SectionThree";
 import Footer from "@/components/Footer/Footer";
-import Tilt from 'react-parallax-tilt';
+import Image from "next/image";
+import ArrowRight from "./assets/svgs/arrow_right.svg";
+import { TypeAnimation } from "react-type-animation";
+
 
 const helvetic700 = localFont({
   src: './assets/font/HelveticaNeueBold.otf'
@@ -35,27 +36,54 @@ export default function Home() {
       >
         <div className="container">
           <NavigationNavbar />
-          <div className="hero">
-            <div className="wrapper">
-              <div className="wrapper_left">
-                <h1 className={`wrapper_title ${helvetic700.className}`}>Embrace innovation,<br /> optimize processes,<br />  and enhance customer<br /> engagement through<br /> digital transformation</h1>
-                <Flex gap="small" wrap className="flex_btn">
+        </div>
+        <div className="hero">
+            <div className="main-content">
+              <div className="container">
+                <h1 className={`${helvetic700.className} hero_title`}>Al Andalus</h1>
+                {/* <TypeAnimation
+                  sequence={[
+                    'Al Andalus'
+                  ]}
+                  wrapper="h1"
+                  speed={50}
+                  repeat={1}
+                  className={`${helvetic700.className} hero_title`}
+                  cursor={false}
+                /> */}
+                <p className={`hero_text ${helvetic700.className}`}>Innovate, optimize processes, enhance customer engagement.</p>
+              </div>
+              <Image src={require("./assets/section.png")} alt="" className="hero_image" />
+              <div className="content">
+                <TypeAnimation
+                  preRenderFirstString={true}
+                  sequence={[
+                    'At AL Andalus, we deliver innovative IT solutions tailored to your business. Located in Dubai, we specialize in comprehensive digital transformation, optimizing processes, and enhancing customer engagement.',
+                    1000,
+                    'At AL Andalus, we deliver innovative IT solutions tailored to your business. Located in Dubai, we specialize in comprehensive digital transformation, optimizing processes, and boosting client interactions.',
+                    1000,
+                    'At AL Andalus, we deliver innovative IT solutions tailored to your business. Located in Dubai, we specialize in comprehensive digital transformation, optimizing processes, and improving customer connections.',
+                    1000,
+                    'At AL Andalus, we deliver innovative IT solutions tailored to your business. Located in Dubai, we specialize in comprehensive digital transformation, optimizing processes, and elevating user experiences.',
+                  ]}
+                  wrapper="p"
+                  speed={50}
+                  repeat={Infinity}
+                  className={`content_text ${helvetic700.className}`}
+                />
+                <div className="flex_btn">
                   <button className={`btn_hero ${helvetic700.className}`}><span className={helvetic700.className}>Let’s Talk</span></button>
-                  <div className="btn_two" >
-                    <button onClick={() => handleSmooth()} className={`btn_hero_text ${helvetic700.className}`}><span className={helvetic700.className}>See works</span></button>
+                  <div onClick={() => handleSmooth()} className={`${helvetic700.className} btn_two`} >
+                   See works
                     <div onClick={() => handleSmooth()} className="arrow_btn">
                       <img src={ArrowRight.src} alt="arrow-right" />
                     </div>
                   </div>
-                </Flex>
-              </div>
-              <div className="wrapper_right">
-                <Tilt>
-                  <Image src={require("./assets/arrowUpImage.png")} alt="Embrace innovation, optimize processes, and enhance customer engagement through digital transformation" className={"hero_img"} />
-                </Tilt>
+                </div>
               </div>
             </div>
           </div>
+        <div className="container">
           <SectionCarousel />
           <SectionList />
           <SectionThree />
